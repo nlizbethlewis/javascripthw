0b44e4e0b0a28cb3cff5894c373663de272657e6
NICOLE LEWIS <nicole.lewis@journalism.cuny.edu>
2016-02-14 16:45:43 -0500

    all files

---
+++ javascript.js
@@ -0,0 +1,13 @@
+var thisOne = 25;
+var thatOne = 100;
+
+var addition = thisOne + thatOne;
+var subtraction = thisOne - thatOne;
+var multiply = thisOne * thatOne;
+var divide = thisOne / thatOne;
+
+
+document.getElementById("addme").innerHTML= "When I add " +thisOne+ " and " +thatOne+ " it equals: " +addition;
+document.getElementById("subtractme").innerHTML= "When I subtract " +thisOne+ " and " +thatOne+ " it equals: " +subtraction;
+document.getElementById("multiplyme").innerHTML= "When I multiply " +thisOne+ " and " +thatOne+ " it equals: " +multiply;
+document.getElementById("divideme").innerHTML= "When I divide " +thisOne+ " and " +thatOne + " it equals: " +divide; 
